66a3a43a98e349f6ff8ceba861702274e04a0c69
gjz010 <gjz010944@gmail.com>
2020-06-24 21:08:29 +0800

    renaming arbit stuff

--- src/Typecore/Utils.hs
+++ src/Typecore/Utils.hs
@@ -1,14 +1,17 @@
 module Typecore.Utils where
 import Clash.Sized.BitVector
-import Clash.Prelude
 import GHC.TypeLits.Induction
 import qualified Clash.Class.BitPack as BitPack
 import qualified Clash.Sized.Vector as V
 import Clash.Prelude
 import Data.Proxy
+import Data.Maybe
 
 --slice :: (KnownNat a, KnownNat size)=>BitVector a->Int->BitVector size
 --slice vec offset = resize (vec `shiftR` offset)
+
+
+
 
 priorityEncoder :: (KnownNat n, (1 <=? n) ~ True)=>BitVector n->BitVector (CLog 2 n)
 priorityEncoder = BitPack.pack . priorityEncoder' . BitPack.unpack 
@@ -31,26 +34,35 @@ priorityOneHot = toPriorityEncoder $
                             then b ++# 0
                             else b ++# (toPriorityEncoder encoder rest))
         (PriorityOneHot id)
-arbit :: (KnownNat n, KnownNat (BitSize a), BitPack a)=>BitVector n->Vec n a->a
-arbit i v=
+
+-- Merging arbit result.
+mergeBV :: (KnownNat a, KnownNat b)=>Vec a (BitVector b)->BitVector b
+mergeBV = V.v2bv . (V.map reduceOr) . V.transpose . (V.map V.bv2v)
+
+mergeBy :: (KnownNat n, KnownNat (BitSize a), BitPack a)=>BitVector n->Vec n a->a
+mergeBy i v=
     let pv=map pack v
         vi=bv2v i
         iv=zip vi pv
         fv=map (\(a, b)->if a==1 then b else 0) iv
-        fbv=map bv2v fv
-        fbvt=transpose fbv
-        finalt=map reduceOr fbvt
-    in unpack $ v2bv finalt
+    in unpack $ mergeBV fv
 
--- Merging multiple bitvectors using or-operation.
-transposeVector :: Vec a (Vec b s)->Vec b (Vec a s)
-transposeVector (V.Cons V.Nil _ )= Nil
-transposeVector x@(V.Cons (V.Cons a b) xs)  = V.Cons (V.map V.head x) (transposeVector (V.map V.tail x))
 
-mergeResult :: (KnownNat a, KnownNat b)=>Vec a (BitVector b)->BitVector b
-mergeResult = V.v2bv . (V.map reduceOr) . transposeVector . (V.map V.bv2v)
+mergeMaybe :: (KnownNat n, KnownNat (BitSize a), BitPack a)=>Vec n (Maybe a)->Maybe a
+mergeMaybe v = let bs = v2bv $ V.map (\x->if isJust x then 1 else 0) v in mergeBy bs v
 
-setMask :: (KnownNat a, KnownNat b, (1<=?b) ~ True)=>proxy b->BitVector (DivRU a b)->BitVector a->BitVector a->BitVector a
+
+arbitWith :: (KnownNat n, KnownNat (BitSize a), BitPack a)=>(BitVector n->BitVector n)->Vec n (Maybe a)->Maybe a
+arbitWith f v = let bs = v2bv $ V.map (\x->if isJust x then 1 else 0) v in mergeBy (f bs) v
+
+arbitPriority :: (KnownNat n, KnownNat (BitSize a), BitPack a)=>Vec n (Maybe a)->Maybe a
+arbitPriority = arbitWith (priorityOneHot)
+
+-- Arbit with round robin.
+arbitRoundRobin :: (HiddenClockResetEnable dom, KnownNat n, KnownNat (BitSize a), BitPack a)=>Signal dom (Vec n (Maybe a))->Signal dom (Maybe a)
+arbitRoundRobin = undefined
+
+setMask :: (KnownNat a, KnownNat b, (1<=?b) ~ 'True)=>proxy b->BitVector (DivRU a b)->BitVector a->BitVector a->BitVector a
 setMask (p :: proxy b) mask x y = 
     let byte_mask = resize $ V.v2bv $ V.concat $ V.map (V.bv2v . (\x->((if x==1 then (-1) else 0) :: BitVector b))) $ V.bv2v mask
         neg_mask = complement byte_mask
